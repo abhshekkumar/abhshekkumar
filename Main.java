7b1011644ef47d5effa9a0dcac14c2406653846c
abhshekkumar <141306292+abhshekkumar@users.noreply.github.com>
2023-08-03 14:03:28 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Mult {
+	public static void main(String[] args)
+	{
+		// initializing variables
+		int num1 = 11, num2 = 9, mult = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// Remaindering num1 and num2
+		mult = num1 * num2;
+		System.out.println("addition="+mult);
+	}
+}
